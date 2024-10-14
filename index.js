12af31eba2bd39113c62600b1c07adb92bded489
black-desk <me@black-desk.cn>
2024-10-14 16:44:04 +0800

    fixup! feat: apply title from pandoc metadata block

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let path = require('node:path');
 let os = require('node:os');
 let fs = require('node:fs');
@@ -14,46 +16,7 @@ function pandocRenderer(data, options){
 
   let src = data.text.toString();
 
-  let standalone = "path" in data;
-
-  let folder = '';
-
-  if (standalone) {
-    folder = fs.mkdtempSync(path.join(os.tmpdir() , 'hexo-renderer-pandoc-'));
-    fs.writeFileSync(path.join(folder, '/meta-json.tpl'), "$meta-json$");
-    args.push("--template", folder + '/meta-json.tpl')
-  }
-
   let res = spawnSync(pandoc_path, args, {
-    cwd: process.cwd(),
-    env: process.env,
-    encoding: "utf8",
-    input: src
-  });
-
-  if (standalone) {
-    // fs.rmSync(folder);
-    args.splice(args.indexOf("--template"), 2);
-  }
-
-  if (res.status === 0) {
-    if (res.stderr) {
-      let warn_msg = ''
-        + '[WARNING][hexo-renderer-pandoc] On ' + data.path + '\n'
-        + '[WARNING][hexo-renderer-pandoc] ' + res.stderr;
-      console.log(warn_msg);
-    }
-
-    data.title = JSON.parse(res.stdout).title;
-  } else {
-    let error_msg = '\n'
-      + '[ERROR][hexo-renderer-pandoc] On ' + data.path + '\n'
-      + '[ERROR][hexo-renderer-pandoc] pandoc exited with code '+res.status+(res.stderr ? ': ' + res.stderr : '.');
-    console.log(error_msg);
-    throw Error(error_msg);
-  }
-
-  res = spawnSync(pandoc_path, args, {
     cwd: process.cwd(),
     env: process.env,
     encoding: "utf8",
@@ -77,6 +40,45 @@ function pandocRenderer(data, options){
     throw Error(error_msg);
   }
 }
+
+hexo.extend.filter.register('before_post_render', (data) => {
+  let standalone = "path" in data;
+  if (!standalone) {
+    return
+  }
+
+  let folder = '';
+
+    folder = fs.mkdtempSync(path.join(os.tmpdir() , 'hexo-renderer-pandoc-'));
+    fs.writeFileSync(path.join(folder, '/meta-json.tpl'), "$meta-json$");
+    args.push("--template", folder + '/meta-json.tpl')
+
+  let res = spawnSync(pandoc_path, args, {
+    cwd: process.cwd(),
+    env: process.env,
+    encoding: "utf8",
+    input: src
+  });
+
+  if (res.status === 0) {
+    if (res.stderr) {
+      let warn_msg = ''
+        + '[WARNING][hexo-renderer-pandoc] On ' + data.path + '\n'
+        + '[WARNING][hexo-renderer-pandoc] ' + res.stderr;
+      console.log(warn_msg);
+    }
+
+    data.title = JSON.parse(res.stdout).title;
+  } else {
+    let error_msg = '\n'
+      + '[ERROR][hexo-renderer-pandoc] On ' + data.path + '\n'
+      + '[ERROR][hexo-renderer-pandoc] pandoc exited with code '+res.status+(res.stderr ? ': ' + res.stderr : '.');
+    console.log(error_msg);
+    throw Error(error_msg);
+  }
+
+  console.log(data.source + " Title found: " + title[1]);
+});
 
 hexo.extend.renderer.register('md', 'html', pandocRenderer, true);
 hexo.extend.renderer.register('markdown', 'html', pandocRenderer, true);
